61bcc0627eb425310e84f906e7f60da513f3b0be
Guilherme Neves <guilhermes_neves@hotmail.com>
2020-07-13 18:54:09 -0300

    Configurando o jenkins

--- escritorio/features/support/libs/database.rb
+++
@@ -1,7 +0,0 @@
-require "mysql2"
-
-class DataBase
-  def update_order(orderId)
-    @client.query("UPDATE hlg_integrador.Pedidos set StatusPagamento = '5' WHERE PedidoId = '#{orderId}';")
-  end
-end
